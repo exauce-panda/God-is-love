bf785fa5e4ff161883725d27503e3966a7b42c4d
exauce-panda <2706053@collegelacite.ca>
2025-07-14 17:38:33 -0400

    youtubr

--- components/LatestVideo.jsx
+++ components/LatestVideo.jsx
@@ -1,7 +1,6 @@
-'use client'
 import { useEffect, useState } from "react";
 import styles from './LatestVideo.module.css'
-
+/*'use client'
 export default function LatestVideos() {
   const [videos, setVideos] = useState([]);
 
@@ -23,4 +22,31 @@ export default function LatestVideos() {
       ))}
     </div>
   );
+}*/
+export default function LatestVideos() {
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    fetch("/api/youtube")
+      .then((res) => res.json())
+      .then((data) => setVideos(data))
+      .catch((error) => console.error("Erreur de récupération des vidéos :", error));
+  }, []);
+
+  if (!videos) {
+    return null; // ou un loader
+  }
+
+  return (
+    <div className={styles.affichage}>
+      {videos.map((video) => (
+        <div key={video.id.videoId} className="p-4 rounded-lg">
+          <a href={`https://www.youtube.com/watch?v=${video.id.videoId}`} target="_blank" rel="noopener noreferrer">
+            <img src={video.snippet.thumbnails.medium.url} alt={video.snippet.title} className="w-full" />
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 }
+
