296c39074a43451032df6e42d50febd28f134fd2
exauce-panda <2706053@collegelacite.ca>
2025-08-26 18:37:31 -0400

    metadata (SEO)

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,19 @@
-import Home from './home'
-
+// Importation du composant Home depuis le fichier home.jsx
+import Home from './home';
+// Objet metadata pour stocker les métadonnées de la page, telles que le titre et la description
+// Ces métadonnées peuvent être utilisées pour améliorer le SEO de la page
+/**
+ * @type {import("next").Metadata}
+ */
+export const metadata = {
+  title: 'Eglise GODISLOVECENTER',
+  description: 'Nous sommes une Eglise pronant l amour de Dieu manifesté pour nous',
+  openGraph: {
+    title: 'Eglise GODISLOVECENTER',
+    description: 'nous sommes une seule et grande famille. Nous expérimentons l amour de Dieu, car il nous aime plus que tout. Nous reconnaissons que Jésus-Christ est Dieu et qu il est mort pour nos péchés.',
+    images: ['/Logos/logoGilc.png']
+  }
+}
 export default function Page() {
   return (
        <div>
